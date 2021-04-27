feba232672ec0fa141fa6a45b68afbe0b17a791e
camerondavis3245 <camerondavis3245@gmail.com>
2021-04-27 03:22:22 -0500

    JUnit Test for database

---
+++ Asteroids/database/DatabaseTest.java
@@ -0,0 +1,44 @@
+package database;
+
+import static org.junit.Assert.*;
+
+import java.sql.SQLException;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import database.Database;
+
+public class DatabaseTest
+{
+    private Database database;
+    
+    // Will read database properties, getConnection
+    @Before
+    public void setUpDatabase() throws Exception
+    {
+        database = new Database();
+    }
+    
+    @Test 
+    public void testCreateAccount()
+    {
+        String username = "JaneDoe";
+        String password = "password";
+        boolean oldAccount = database.createNewAccount(username, password);
+        assertTrue("Account created already", oldAccount);
+        
+    }
+    @Test
+    public void verifyAccount()
+    {
+        String username = "JaneDoe";
+        String password = "password";
+        boolean found = database.verifyAccount(username, password);
+        
+        if (!found)
+        {
+            fail("Account not found/Does not exist");
+        }
+    }
+}
